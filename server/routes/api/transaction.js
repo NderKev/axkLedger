f9a1e044d2f038ad6a99c6fbf47d7eb102806b73
NderKev <nderebakelvin@gmail.com>
2024-08-08 16:51:14 +0300

    axk-ledger-server-set-up-fix

--- server/routes/api/transaction.js
+++ server/routes/api/transaction.js
@@ -4,16 +4,18 @@ const { check } = require('express-validator');
 //const {} = require('../../validators/address');
 const {validateTokenTransaction} = require('../../middleware/auth');
 
-const {  addTransaction, getCurrentTransaction } = require('../../controllers/transaction');
+const {  addTransaction, getTransaction } = require('../../controllers/transaction');
 
-router.get('/', validateTokenTransaction, getCurrentTransaction);
+router.get('/', validateTokenTransaction, getTransaction);
 
 router.post(
     '/',
     [
+      check('wallet_id', 'Please include a wallet id').exists(),
       check('address', 'Please include an address').exists(),
       check('tx_hash', 'tx_hash is required').exists(),
       check('type', 'Please include the type').exists(),
+      check('mode', 'Please include the mode').exists(),
       check('to', 'to is required').exists(),
       check('value', 'value is required').exists(),
     ],
