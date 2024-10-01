b89b27b118990b379c00eadab2b433a4ab4a7818
NderKev <nderebakelvin@gmail.com>
2024-10-01 19:51:07 +0300

    reconfig-server-bd

--- server/psql/config.js
+++ server/psql/config.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env'});
+//dotenv.config({ path: '../../.env'});
 //dotenv.config({ path: './local.env'});
 //Load env vars if env is not production
 
