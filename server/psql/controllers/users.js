49b078af40efb850699111db8a74c2a46361085c
NderKev <nderebakelvin@gmail.com>
2024-10-13 01:28:11 +0300

    fix-gen-otp

--- server/psql/controllers/users.js
+++ server/psql/controllers/users.js
@@ -57,10 +57,11 @@ exports.generateUniqueId = function(length){
       await users.createUserToken(token);
       const email_ver_token = await users.genVerToken(input.email);
       //await users.createEmailToken(email_ver_token);
-      const otp = generateOTP(6);
+      const otp = this.generateOTP(6);
       console.log("otp  :" + otp);
+
       await users.createEmailToken({email : email_ver_token.email, expiry : email_ver_token.expiry, token : otp});
-      //email_ver_token.token = otp;
+      email_ver_token.token = otp;
      // const link = `${req.protocol}://${req.get('host')}${req.originalUrl}/verify/${email_ver_token.token}`;
       //const bd_link = `http://102.133.149.187/backend/users/verify/${email_ver_token.token}`
       const link = `http://102.133.149.187/backend/users/otp/${otp}`;
@@ -425,7 +426,7 @@ exports.generateUniqueId = function(length){
   };
 
 
-  const generateOTP = (length)=> {
+  exports.generateOTP = (length)=> {
     const characters = '0123456789';
     let lot_num = '';
     for (let i = 0; i < length; i++) {
